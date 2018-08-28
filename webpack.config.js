de7565d2cb6ec3ca1d6b3ebf30e84630fac739a4
iyonaga <yiyonaga@gmail.com>
2018-08-28 20:32:58 +0900

    Add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const isProduction = !process.env.WEBPACK_SERVE;
 
@@ -122,3 +123,7 @@ module.exports = {
     open: true
   }
 };
+
+if (isProduction) {
+  module.exports.plugins.push(new CleanWebpackPlugin(['dist']));
+}
