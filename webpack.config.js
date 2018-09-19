935812d0ccf6a5c02169100b1165f1eec39be141
iyonaga <yiyonaga@gmail.com>
2018-09-20 02:14:55 +0900

    Change uglifyjs-webpack-plugin to terser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const chokidar = require('chokidar');
 
@@ -110,8 +110,9 @@ module.exports = {
     },
     minimizer: isProduction
       ? [
-          new UglifyJSPlugin({
-            uglifyOptions: {
+          new TerserPlugin({
+            parallel: true,
+            terserOptions: {
               compress: {
                 drop_console: true
               }
